477b78ea38979bd4fc1c4a62d19cb2382c8b3a02
phort6n2 <158865829+phort6n2@users.noreply.github.com>
2024-05-18 16:55:57 -0600

    d

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,6 @@ const configSitemap = require("./src/config/sitemap");
 const configServer = require("./src/config/server");
 
 const filterPostDate = require("./src/config/postDate");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
     // EXTENSIONS - Recognising non-default languages as templates
@@ -26,14 +25,10 @@ module.exports = function (eleventyConfig) {
     // https://www.npmjs.com/package/@quasibit/eleventy-plugin-sitemap
     eleventyConfig.addPlugin(pluginSitemap, configSitemap);
 
-    eleventyConfig.addPlugin(pluginRss);
-
     // When in production ("npm run build" is ran), minify all HTML, CSS, JSON, XML, XSL and webmanifest files.
     // https://github.com/benjaminrancourt/eleventy-plugin-files-minifier
     if (configServer.isProduction) {
         eleventyConfig.addPlugin(pluginMinifier);
-            
-        };    
     }
     // END PLUGINS
 
